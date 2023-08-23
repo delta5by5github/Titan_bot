ceea087146e0d4474913c59f01da98021a1a716f
delta5by5github <113343756+delta5by5github@users.noreply.github.com>
2023-08-23 16:03:28 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " - " + json_data[0]['a']
+    return quote
+
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+try:
+    client.run("#")  # Replace with your bot token
+except discord.HTTPException as e:
+    if e.status == 429:
+        print("The Discord servers denied the connection for making too many requests")
+        print("Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests")
+    else:
+        raise e
